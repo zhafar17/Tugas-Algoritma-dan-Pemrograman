0022631686a2dd89c06c8eae6af1d4e4108cabcf
zhafar17 <62021577+zhafar17@users.noreply.github.com>
2020-03-30 12:56:32 +0800

    Add files via upload

---
+++ program palindrom.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	char text1[]="kodok";
+	char text2[]="tamat";
+	char text3[]="radar";
+	
+	strlen(text1);
+	cout<<text1<<endl;
+	
+	strlen(text2);
+	cout<<text2<<endl;
+	
+	strlen(text3);
+	cout<<text3<<endl;
+}
+
---
+++ tukar bilangan desimal ke biner.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+void binari (int desimal){
+	int sisa, hasil;
+	if (desimal<=1){
+		cout<<desimal;
+		return ;
+	}
+	sisa=desimal%2;
+	hasil=desimal/2;
+	binari(hasil);
+	cout<<sisa;
+}
+int main()
+{
+	int angka, biner, sisa, hasil, n;
+	cout<<"masukkan angka desimal=";
+	cin>>angka;
+	cout<<"angka binarinya adalah=";
+	binari(angka);
+	
+}
+
